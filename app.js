cf2dc2e5927089479450192ce8955dcac482d3bf
MuammadAnusAhmed <MUHAMMADANUSAHMEDON@gmail.com>
2024-04-23 18:07:46 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,36 @@
+"use strict";
+// Question22: intentional error
+var friends = ["Alice", "Bob", "Charlie"];
+console.log(friends[3]); // Intentional error: Arrays are zero-indexed, so index 3 is out of bounds.
+friends[2] = "Charlie"; // Correcting the error by accessing a valid index.
+console.log(friends[2]);
+// Question23: conditional error
+var car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'subaru'); // True
+console.log("Is car == 'toyota'? I predict False.");
+console.log(car == 'toyota'); // False
+// Question23: more conditional error
+//Equality with strings
+console.log("Testing equality with strings:");
+console.log("apple" == "apple"); // True
+// console.log("apple" == "Apple"); // False
+// Using the lower case function
+console.log("Testing with lower case:");
+console.log("Apple".toLowerCase() == "apple"); // True
+// Numerical tests
+console.log("Numerical test");
+console.log(10 > 2); //true
+console.log(10 < 2); //false
+// Tests using "and" and "or" operators
+console.log("Tests with 'and' and 'or':");
+console.log(true && false); // False
+console.log(true || false); // True
+// test whether an item is in a array  // use include
+var fruit = ["apple", "banana", "cherry"];
+console.log("is 'apple' in fruit?");
+console.log(fruit.includes("apple")); // true
+// test whether an item is in a array  // use include
+console.log("is 'mango' not in fruit?");
+console.log(!fruit.includes("mango")); // true  
+// because we are use  ! not equal sign that's a reason it's give me true 
